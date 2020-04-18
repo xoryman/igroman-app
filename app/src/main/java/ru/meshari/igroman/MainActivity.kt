334a66fa72d6f0ab97f4d2a65b73564d90b27365
howtus <howtus12@gmail.com>
2020-04-18 23:15:03 +0300

    Release 1.2.2

--- app/src/main/java/ru/meshari/igroman/MainActivity.kt
+++ app/src/main/java/ru/meshari/igroman/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.meshari.igroman
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,23 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnTruthOrDrink.setOnClickListener {
+        mainGameCount1.text = "Вопросов: " + resources.getStringArray(R.array.questions).size.toString()
+        mainGameCount2.text = "Вопросов: " + resources.getStringArray(R.array.strings).size.toString()
+        mainGameCount3.text = "Слов: " + resources.getStringArray(R.array.words).size.toString()
+
+        mainGameDiv1.setOnClickListener {
             val intent = Intent(this, TordActivity::class.java)
             startActivity(intent)
         }
 
-        btnINeverDoIt.setOnClickListener {
+        mainGameDiv2.setOnClickListener {
             val intent = Intent(this, NeverActivity::class.java)
             startActivity(intent)
         }
 
-        btnCrocodile.setOnClickListener {
+        mainGameDiv3.setOnClickListener {
             val intent = Intent(this, CrocodileActivity::class.java)
             startActivity(intent)
         }
-        btnMovie.setOnClickListener {
-            Toast.makeText(this, "Игра временно недоступна", Toast.LENGTH_SHORT).show()
+
+        mainGameDiv4.setOnClickListener {
+            Toast.makeText(this, "Игра еще в разработке", Toast.LENGTH_SHORT).show()
         }
+
 
     }
 
